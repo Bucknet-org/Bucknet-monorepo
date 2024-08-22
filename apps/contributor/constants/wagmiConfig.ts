b82feedd9baedb987864e19127c989e7adac6b9b
Bekh-nam <bekhnam2021@gmail.com>
2024-08-23 04:29:45 +0700

    add wagmi

---
+++ apps/contributor/constants/wagmiConfig.ts
@@ -0,0 +1,13 @@
+import { http, createConfig } from 'wagmi'
+import { bscTestnet } from 'wagmi/chains'
+import { metaMask } from 'wagmi/connectors'
+
+export const wagmiConfig = createConfig({
+  chains: [bscTestnet],
+  connectors: [
+    metaMask()
+  ],
+  transports: {
+    [bscTestnet.id]: http(),
+  },
+})
--- apps/contributor/pages/_app.tsx
+++ apps/contributor/pages/_app.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { wagmiConfig } from "@/constants/wagmiConfig";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import ConnectWallet from "@/components/ConnectWallet";
+
+const queryClient = new QueryClient()
+
 interface AppProps {
   Component: any,
   pageProps: any,
@@ -6,7 +13,12 @@ interface AppProps {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
