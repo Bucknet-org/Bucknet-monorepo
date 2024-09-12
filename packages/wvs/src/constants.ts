f9a9d1ce32e9a25917984dc1cc0fcc528bd9987e
Bekhnam <bekhnam2021@gmail.com>
2024-09-12 13:55:10 +0700

    open WVS - epoch 1

--- packages/wvs/src/constants.ts
+++ packages/wvs/src/constants.ts
@@ -1,2 +1,6 @@
 export const RPC_URL = "https://bsc-testnet.blockpi.network/v1/rpc/public";
-export const CONTRIBUTOR_ADDRESS = '0x4352c561dc37F456923f5282Ff30862E15Bd6DD6';
+
+//test address
+// export const CONTRIBUTOR_ADDRESS = '0x1aD5cc73a2Fa1DC716929453D5FC71D52809a3f0';
+
+export const CONTRIBUTOR_ADDRESS = '0x57b4009a9E02C2E7e0282B0944B83Cc9D61e6c5d';
